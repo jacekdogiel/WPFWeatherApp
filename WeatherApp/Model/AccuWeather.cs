4796f945f9c0c00470fac5fff96f1a6e2e872c68
jacek.dogiel@gmail.com <jacek.dogiel@gmail.com>
2019-04-13 19:52:09 +0200

    Add project files.

---
+++ WeatherApp/Model/AccuWeather.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WeatherApp.Model
+{
+    public class Range
+    {
+        public double Value { get; set; }
+        public string Unit { get; set; }
+        public int UnitType { get; set; }
+    }
+
+    public class Temperature
+    {
+        public Range Minimum { get; set; }
+        public Range Maximum { get; set; }
+    }
+
+    public class TimeOfDay
+    {
+        public int Icon { get; set; }
+        public string IconPhrase { get; set; }
+    }
+
+    public class DailyForecast
+    {
+        public DateTime Date { get; set; }
+        public Temperature Temperature { get; set; }
+        public TimeOfDay Day { get; set; }
+        public TimeOfDay Night { get; set; }
+        public List<string> Sources { get; set; }
+    }
+
+    public class AccuWeather
+    {
+        public List<DailyForecast> DailyForecasts { get; set; }
+    }
+}
---
+++ WeatherApp/ViewModel/WeatherAPI.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System.Net.Http;
+using System.Threading.Tasks;
+using WeatherApp.Model;
+
+namespace WeatherApp.ViewModel
+{
+    public class WeatherAPI
+    {
+        public const string API_KEY = "ohXrTBlqBXIDxPGZq5djbbfVLo3QBkTa";
+        public const string BASE_URL = "http://dataservice.accuweather.com/forecasts/v1/daily/5day/{0}?apikey={1}&metric=true";
+
+        public static async Task<AccuWeather> GetWeatherInformationAsync(string locationKey)
+        {
+            var result = new AccuWeather();
+
+            string url = string.Format(BASE_URL, locationKey, API_KEY);
+
+            using (var client = new HttpClient())
+            {
+                var response = await client.GetAsync(url);
+                var json = await response.Content.ReadAsStringAsync();
+
+                result = JsonConvert.DeserializeObject<AccuWeather>(json);
+            }
+            return result;
+        }
+    }
+}
