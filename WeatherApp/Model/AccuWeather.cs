d7cd1068daf54887b26f9f32c1b0624ed78ecc31
jacek.dogiel@gmail.com <jacek.dogiel@gmail.com>
2019-04-14 13:28:25 +0200

    Implementing INotifyPropertyChanged

--- WeatherApp/Model/AccuWeather.cs
+++ WeatherApp/Model/AccuWeather.cs
@@ -1,38 +1,212 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 
 namespace WeatherApp.Model
 {
-    public class Range
+    public class Range : INotifyPropertyChanged
     {
-        public double Value { get; set; }
-        public string Unit { get; set; }
-        public int UnitType { get; set; }
+        private double value;
+
+        public double Value
+        {
+            get { return value; }
+            set
+            {
+                this.value = value;
+                OnPropertyChanged("Value");
+            }
+        }
+
+        private string unit;
+
+        public string Unit
+        {
+            get { return unit; }
+            set
+            {
+                unit = value;
+                OnPropertyChanged("Unit");
+            }
+        }
+
+        private int unitType;
+
+        public int UnitType
+        {
+            get { return unitType; }
+            set
+            {
+                unitType = value;
+                OnPropertyChanged("UnitType");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 
-    public class Temperature
+    public class Temperature : INotifyPropertyChanged
     {
-        public Range Minimum { get; set; }
-        public Range Maximum { get; set; }
+        private Range minimum;
+
+        public Range Minimum
+        {
+            get { return minimum; }
+            set
+            {
+                minimum = value;
+                OnPropertyChanged("Minimum");
+            }
+        }
+
+        private Range maximum;
+
+        public Range Maximum
+        {
+            get { return maximum; }
+            set
+            {
+                maximum = value;
+                OnPropertyChanged("Maximum");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 
-    public class TimeOfDay
+    public class TimeOfDay : INotifyPropertyChanged
     {
-        public int Icon { get; set; }
-        public string IconPhrase { get; set; }
+        private int icon;
+
+        public int Icon
+        {
+            get { return icon; }
+            set
+            {
+                icon = value;
+                OnPropertyChanged("Icon");
+            }
+        }
+
+        private int iconPhrase;
+
+        public int IconPhrase
+        {
+            get { return iconPhrase; }
+            set
+            {
+                iconPhrase = value;
+                OnPropertyChanged("IconPhrase");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 
-    public class DailyForecast
+    public class DailyForecast : INotifyPropertyChanged
     {
-        public DateTime Date { get; set; }
-        public Temperature Temperature { get; set; }
-        public TimeOfDay Day { get; set; }
-        public TimeOfDay Night { get; set; }
         public List<string> Sources { get; set; }
+
+        private DateTime date;
+
+        public DateTime Date
+        {
+            get { return date; }
+            set
+            {
+                date = value;
+                OnPropertyChanged("Date");
+            }
+        }
+
+        private Temperature temperature;
+
+        public Temperature Temperature
+        {
+            get { return temperature; }
+            set
+            {
+                temperature = value;
+                OnPropertyChanged("Temperature");
+            }
+        }
+
+        private TimeOfDay day;
+
+        public TimeOfDay Day
+        {
+            get { return day; }
+            set
+            {
+                day = value;
+                OnPropertyChanged("Day");
+            }
+        }
+
+        private TimeOfDay night;
+
+        public TimeOfDay Night
+        {
+            get { return night; }
+            set
+            {
+                night = value;
+                OnPropertyChanged("Night");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 
     public class AccuWeather
     {
         public List<DailyForecast> DailyForecasts { get; set; }
+
+        public AccuWeather()
+        {
+            if (DesignerProperties.GetIsInDesignMode(new System.Windows.DependencyObject()))
+            {
+                DailyForecasts = new List<DailyForecast>();
+                for (int i = 0; i < 3; i++)
+                {
+                    DailyForecast dailyForecast = new DailyForecast
+                    {
+                        Date = DateTime.Now.AddDays(-i),
+                        Temperature = new Temperature
+                        {
+                            Maximum = new Range
+                            {
+                                Value = 21 + i
+                            },
+                            Minimum = new Range
+                            {
+                                Value = 5 - i
+                            }
+                        }
+                    };
+                    DailyForecasts.Add(dailyForecast);
+                }
+            }
+        }
     }
 }
