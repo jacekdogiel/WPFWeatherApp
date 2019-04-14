5ea7598f7b40f4ff5f6d22d46fd168d9e201ee17
jacek.dogiel@gmail.com <jacek.dogiel@gmail.com>
2019-04-14 15:10:34 +0200

    Added City Model

---
+++ WeatherApp/ViewModel/WeatherVM.cs
@@ -0,0 +1,14 @@
+﻿using WeatherApp.Model;
+
+namespace WeatherApp.ViewModel
+{
+    public class WeatherVM
+    {
+        public AccuWeather Weather { get; set; }
+
+        public WeatherVM()
+        {
+            Weather = new AccuWeather();
+        }
+    }
+}
